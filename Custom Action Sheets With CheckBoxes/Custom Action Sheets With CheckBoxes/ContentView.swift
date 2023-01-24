aabf5e1075dc19c50db8b55dc622112820985242
Elaidzha1940 <eli080293@gmail.com>
2023-01-24 18:36:43 +0700

    Custom Action Sheets With CheckBoxes - SwiftUI Checkbox

---
+++ Custom Action Sheets With CheckBoxes/Custom Action Sheets With CheckBoxes/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Custom Action Sheets With CheckBoxes
+//
+//  Created by Elaidzha Shchukin on 24.01.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct 
---
+++ Custom Action Sheets With CheckBoxes/Custom Action Sheets With CheckBoxes/Custom_Action_Sheets_With_CheckBoxesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Custom_Action_Sheets_With_CheckBoxesApp.swift
+//  Custom Action Sheets With CheckBoxes
+//
+//  Created by Elaidzha Shchukin on 24.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct Custom_Action_Sheets_With_CheckBoxesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
